56d219a90fb460a5cbfe52d8468448247270a4cc
Dayana <dayanadiasgarcia@gmail.com>
2021-10-15 15:55:51 -0300

    add exercicios do bloco 4 - dia 1

--- fundamentos/bloco4/dia-1/exerc41.js
+++ fundamentos/bloco4/dia-1/exerc41.js
@@ -1,3 +1,5 @@
+//--------------EXERCÍCIOS DO BLOCO 4 DIA 1------------------//
+
 // const myName = "Dayana";
 // const birthCity = "São Paulo";
 // birthCity = "Itapecerica da Serra"
@@ -5,10 +7,14 @@
 // birthYear = 2030;
 // console.log(myName, birthCity, birthYear);
 
+//------------------------------------------
+
 // let patientId = '50';
 // let isEnrolled = true;
 // const patientInfo = {firstName: 'Ana', lastName: 'Santos'}
 // const patientEmail = 'ana@email.com';
+
+//------------------------------------------
 
 // console.log(typeof patientId)
 // console.log(typeof isEnrolled)
@@ -16,11 +22,15 @@
 // console.log(typeof patientInfo)
 // console.log(typeof patientAge)
 
+//------------------------------------------
+
 // const base = 5;
 // let heigth = 8;
 // const area = (base*heigth);
 // const perimeter = (base +heigth +base + heigth);
 // console.log(perimeter, area)
+
+//------------------------------------------
 
 // const nota = 75;
 
@@ -34,6 +44,8 @@
 //     console.log("A nota precisa está entre 1 e 100");
 //   }
 
+//------------------------------------------
+
 // const comida = 'pão na chapa';
 // const bebida = 'suco de maçã';
 
@@ -42,11 +54,14 @@
 // } else {
 //   console.log('Acho que houve um engano com meu pedido');
 // }
+//------------------------------------------
 
 // const conditionOne = true;
 // const conditionTwo = false;
 
 // console.log(conditionOne && conditionTwo);
+
+//------------------------------------------
 
 // const cenouras = true;
 // const leite = true;
@@ -55,10 +70,14 @@
 
 // const listaDeCompras = cenouras && leite && arroz && feijao;
 
+//------------------------------------------
+
 // console.log(true && true); // true
 // console.log(true && false); // false
 // console.log(false && true); // false
 // console.log(false && false); // false
+
+//------------------------------------------
 
 
 // currentHour = 20;
@@ -77,6 +96,8 @@
 // }
 // console.log(message);
 
+//------------------------------------------
+
 // let weekDay  = "sábado";
 
 // if(weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira"){
@@ -85,6 +106,7 @@
 //   console.log("FINALMENTE, descanso merecido UwU");
 // }
 
+//------------------------------------------
 
 // let estado = "aprovado";
 
@@ -102,23 +124,71 @@
 //     console.log("Não se aplica");
 // }
 
+//------------------------------------------
 
+// let a = 5;
+// let b = 5;
 
+// let adicao = a+b;
+// let subtracao = a-b;
+// let multiplicacao = a*b;
+// let divisao = a/b;
+// let modulo = a**b;
+// console.log(adicao, subtracao, multiplicacao, divisao, modulo);
 
-let a = 5;
-let b = 5;
+//----------------------------------------
 
-let adicao = a+b;
-let subtracao = a-b;
-let multiplicacao = a*b;
-let divisao = a/b;
-let modulo = a**b;
-console.log(adicao, subtracao, multiplicacao, divisao, modulo);
+// let a = 50;
+// let b = 21;
 
-if(a > b){
-  console.log("O maior é o valor de a:" + a);
-}else if(a < b){
-  console.log("O maior é o valor de b: " + b);   
-}else if (a == b){
-  console.log ("valores iguais!!!");
-}
+// if(a > b){
+//   console.log("O maior é o valor de a: " + a);
+// }else if(a < b){
+//   console.log("O maior é o valor de b: " + b);   
+// }else if (a == b){
+//   console.log ("valores iguais!!!");
+// }
+
+//------------------------------------------
+// const a = 150;
+// const b = 21;
+// const c = 83;
+
+// if(a > b && a > c){
+//   console.log("O maior é o valor de a: " + a);
+// }else if(b < a && b > c){
+//   console.log("O maior é o valor de b: " + b);   
+// }else if (c > a && c > b){
+//   console.log ("O maior é o valor de c: " + c);
+// }
+
+//------------------------------------------
+
+// const valor = 0;
+
+// if(valor > 1){
+//   console.log("Positive");
+// }else if(valor < 0){
+//   console.log("Negative");
+// }else{
+//   console.log("Zero");
+// }
+
+//------------------------------------------
+
+// const a = 100;
+// const b = 5;
+// const c = 75;
+// let sum = a + b + c;
+
+// if(a < 0 || b < 0 || c < 0){
+//   console.log ("O valor dos angulos precisa ser positivo");
+// }
+// if(sum == 180){
+//   console.log(true);
+// }else{
+//   console.log(false);
+// }
+
+//------------------------------------------
+
